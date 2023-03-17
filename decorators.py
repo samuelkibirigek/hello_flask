00dddd4f9bb3bd17b77b5dee528311a67313b78b
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-03-17 18:13:52 +0300

    understanding decorators and how they work

---
+++ decorators.py
@@ -0,0 +1,28 @@
+import time
+
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        function()
+        function()
+    return wrapper_function
+
+
+@delay_decorator
+def say_hello():
+    print("Hello")
+
+
+@delay_decorator
+def say_goodbye():
+    print("Bye")
+
+
+def say_greeting():
+    print("How are you!")
+
+say_hello()
+say_goodbye()
+say_greeting()
+
+
--- hello.py
+++ hello.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/bye")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, Worlds!</p>"
+
+@app.route("/")
+def say_bye():
+    return "See you later!"
+
+if __name__ == "__main__":
+    app.run()
+
